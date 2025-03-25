e3b70572d9e35f0efebbf6fb249fc55d07a48106
Thanachote Ngamkana <141999802+gbb19@users.noreply.github.com>
2025-03-26 05:19:15 +0700

    feat: add status column to user_courses table for tracking course progress

--- db/migrations/20250322211838_create_user_course_table.up.sql
+++ db/migrations/20250322211838_create_user_course_table.up.sql
@@ -1,8 +1,9 @@
 CREATE TABLE IF NOT EXISTS user_courses
 (
     id        CHAR(36) PRIMARY KEY,
-    user_id   CHAR(36) NOT NULL,
-    course_id CHAR(36) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ,
+    user_id   CHAR(36)                                                NOT NULL,
+    course_id CHAR(36)                                                NOT NULL,
+    status    ENUM ('in_progress', 'completed') DEFAULT 'in_progress' NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
     FOREIGN KEY (course_id) REFERENCES courses (id) ON DELETE CASCADE
 );
